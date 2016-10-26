328eea96cafddc7a5711bac92a5e2a5c05eeecf5
Jean Pimentel <jpimentel@ciandt.com>
2016-10-26 18:15:55 -0200

    Add setuptools

--- xmlarrangementandroid/__init__.py
+++ xmlarrangementandroid/__init__.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
+#
+# <XML Arrangement Rules Generator for Android CodeStyle>
+# Copyright (C) 2016  Jean Pimentel <contato@jeanpimentel.com.br>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 from xml.dom import minidom
 from xml.etree.ElementTree import Element, SubElement, tostring
 
-import sys
+__version__ = '0.1.1'
 
 NS = {
     'android': 'http://schemas.android.com/apk/res/android',
@@ -87,8 +104,12 @@ def toprettyxml(section):
         .replace(u'<?xml version="1.0" ?>\n', '')
 
 
-if __name__ == '__main__':
+def main():
     if len(sys.argv) > 1:
         generate(sys.argv[1])
     else:
         sys.exit('Usage: %s FILE' % sys.argv[0])
+
+
+if __name__ == '__main__':
+    main()
